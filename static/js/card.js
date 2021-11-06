16fcd019b675bd39fe2225a9a2676451fd87d589
LuckShiba <luckshiba@protonmail.com>
2021-11-06 19:58:48 -0300

    tendo certeza que o botão NÃO estará ativado :)

--- static/js/card.js
+++ static/js/card.js
@@ -1,6 +1,7 @@
 export function configureCards(vote) {
     [...document.getElementsByClassName('vote-button')].forEach(button => {
         button.addEventListener('click', () => {
+            vote.updateCPF()
             vote.open(button.dataset.id, button.dataset.name)
         })
     })
